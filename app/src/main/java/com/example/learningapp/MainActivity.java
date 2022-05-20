d6269123f52dfdaf04d9ffda745b405c00d0bdf7
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-20 17:17:43 +0500

    Adding Multiple Activities (Commit 3 Done)

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,32 +2,47 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     Button mod_learn,mod_quiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
 
         mod_learn = findViewById(R.id.mod_learn);
         mod_quiz = findViewById(R.id.mod_quiz);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mod_learn.setOnClickListener(this);
-        mod_quiz.setOnClickListener(this);
+        mod_learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ActivityLearning.class);
+                startActivity(intent);
+            }
+        });
+
+        mod_quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ActivityQuiz.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 
-    @Override
-    public void onClick(View view) {
 
 
 
 
-    }
+
 }
