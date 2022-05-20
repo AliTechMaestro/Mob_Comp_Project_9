e48133e1778ba8f4f9189e4f538dd62842445135
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-20 15:23:16 +0500

    User Interface Folder (Commit 2 Done)

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.learningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button mod_learn,mod_quiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mod_learn = findViewById(R.id.mod_learn);
+        mod_quiz = findViewById(R.id.mod_quiz);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mod_learn.setOnClickListener(this);
+        mod_quiz.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+
+
     }
 }
