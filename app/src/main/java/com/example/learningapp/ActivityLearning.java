10778537976ec71128ab426aa4603c0ca9708315
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-20 20:02:44 +0500

    ReFactoring Multiple Activites (Commit 4 Done)

--- app/src/main/java/com/example/learningapp/ActivityLearning.java
+++ app/src/main/java/com/example/learningapp/ActivityLearning.java
@@ -2,13 +2,30 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityLearning extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
     }
+
+    public void moveForward(View v){
+        Intent intent = new Intent(ActivityLearning.this,MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void movePrev(View v){
+        Intent intent = new Intent(ActivityLearning.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/learningapp/MainActivity2.java
+++ app/src/main/java/com/example/learningapp/MainActivity2.java
@@ -2,13 +2,20 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Activity_C extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_c);
+        setContentView(R.layout.activity_main2);
     }
+
+
+
+
+
 }
