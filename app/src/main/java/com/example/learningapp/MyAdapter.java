65757ad748dd6ce6cc3a53fcfd933f06bc6b1aac
DELL PC <bsef19a525@pucit.edu.pk>
2022-06-04 14:21:38 +0500

    Learning App (Learning Module Customized ListView Version (Landscape) Completed) Commit 11 Done

--- app/src/main/java/com/example/learningapp/MyAdapter.java
+++ app/src/main/java/com/example/learningapp/MyAdapter.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
